4fdb70a8a7c90b523a96504d6458113e4fe4da28
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-05-03 23:42:21 +0530

    Initial commit

---
+++ typings.d.ts
@@ -0,0 +1,64 @@
+// export interface Post {
+//   _id: string
+//   _createdAt: string
+//   title: string
+//   author: {
+//     name: string
+//     image: {
+//       asset: {
+//         url: string
+//       }
+//     }
+//   }
+//   description: string
+//   mainImage: {
+//     asset: {
+//       url: string
+//     }
+//   }
+//   slug: {
+//     current: string
+//   }
+//   body: [object]
+// }
+
+export interface Post {
+  _id: string
+  _createdAt: string
+  title: string
+  author: {
+    name: string
+    image: {
+      asset: {
+        url: string
+      }
+    }
+  }
+  comments: Comment[]
+  description: string
+  mainImage: {
+    asset: {
+      url: string
+    }
+  }
+  slug: {
+    current: string
+  }
+  body: [object]
+}
+
+export interface Comment {
+  _id: string
+  _createdAt: string
+  _rev: string
+  type: string
+  _updatedAt: string
+  approved: boolean
+  comment: string
+  email: string
+  name: string
+  post: {
+    _ref: string
+    _type: string
+  }
+}
